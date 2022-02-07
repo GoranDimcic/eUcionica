a03a279e5568afff99314a9420f4ae8e2c4f0749
Goran Dimcic <dimcic.goran@hotmail.com>
2022-02-07 23:08:54 +0100

    Mongo

--- eUcionica/Form1.Designer.cs
+++ eUcionica/Form1.Designer.cs
@@ -29,13 +29,144 @@ namespace eUcionica
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnX2 = new System.Windows.Forms.Button();
+            this.BtnRegistryStudent = new System.Windows.Forms.Button();
+            this.BtnLoginStudent = new System.Windows.Forms.Button();
+            this.BtnX1 = new System.Windows.Forms.Button();
+            this.BtnRegistryProfessor = new System.Windows.Forms.Button();
+            this.BtnLoginProfessor = new System.Windows.Forms.Button();
+            this.BtnStudent = new System.Windows.Forms.Button();
+            this.BtnProfessor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnX2
+            // 
+            this.BtnX2.BackColor = System.Drawing.Color.Transparent;
+            this.BtnX2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnX2.BackgroundImage")));
+            this.BtnX2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnX2.Location = new System.Drawing.Point(367, 200);
+            this.BtnX2.Name = "BtnX2";
+            this.BtnX2.Size = new System.Drawing.Size(40, 40);
+            this.BtnX2.TabIndex = 21;
+            this.BtnX2.UseVisualStyleBackColor = false;
+            this.BtnX2.Visible = false;
+            this.BtnX2.Click += new System.EventHandler(this.BtnX2_Click);
+            // 
+            // BtnRegistryStudent
+            // 
+            this.BtnRegistryStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegistryStudent.Location = new System.Drawing.Point(317, 155);
+            this.BtnRegistryStudent.Name = "BtnRegistryStudent";
+            this.BtnRegistryStudent.Size = new System.Drawing.Size(150, 40);
+            this.BtnRegistryStudent.TabIndex = 20;
+            this.BtnRegistryStudent.Text = "Sign up";
+            this.BtnRegistryStudent.UseVisualStyleBackColor = true;
+            this.BtnRegistryStudent.Visible = false;
+            this.BtnRegistryStudent.Click += new System.EventHandler(this.BtnRegistryStudent_Click);
+            // 
+            // BtnLoginStudent
+            // 
+            this.BtnLoginStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLoginStudent.Location = new System.Drawing.Point(317, 105);
+            this.BtnLoginStudent.Name = "BtnLoginStudent";
+            this.BtnLoginStudent.Size = new System.Drawing.Size(150, 40);
+            this.BtnLoginStudent.TabIndex = 19;
+            this.BtnLoginStudent.Text = "Login";
+            this.BtnLoginStudent.UseVisualStyleBackColor = true;
+            this.BtnLoginStudent.Visible = false;
+            this.BtnLoginStudent.Click += new System.EventHandler(this.BtnLoginStudent_Click);
+            // 
+            // BtnX1
+            // 
+            this.BtnX1.BackColor = System.Drawing.Color.Transparent;
+            this.BtnX1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnX1.BackgroundImage")));
+            this.BtnX1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnX1.Location = new System.Drawing.Point(117, 200);
+            this.BtnX1.Name = "BtnX1";
+            this.BtnX1.Size = new System.Drawing.Size(40, 40);
+            this.BtnX1.TabIndex = 17;
+            this.BtnX1.UseVisualStyleBackColor = false;
+            this.BtnX1.Visible = false;
+            this.BtnX1.Click += new System.EventHandler(this.BtnX1_Click);
+            // 
+            // BtnRegistryProfessor
+            // 
+            this.BtnRegistryProfessor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegistryProfessor.Location = new System.Drawing.Point(67, 155);
+            this.BtnRegistryProfessor.Name = "BtnRegistryProfessor";
+            this.BtnRegistryProfessor.Size = new System.Drawing.Size(150, 40);
+            this.BtnRegistryProfessor.TabIndex = 15;
+            this.BtnRegistryProfessor.Text = "Sign up";
+            this.BtnRegistryProfessor.UseVisualStyleBackColor = true;
+            this.BtnRegistryProfessor.Visible = false;
+            this.BtnRegistryProfessor.Click += new System.EventHandler(this.BtnRegistryProfessor_Click);
+            // 
+            // BtnLoginProfessor
+            // 
+            this.BtnLoginProfessor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLoginProfessor.Location = new System.Drawing.Point(67, 105);
+            this.BtnLoginProfessor.Name = "BtnLoginProfessor";
+            this.BtnLoginProfessor.Size = new System.Drawing.Size(150, 40);
+            this.BtnLoginProfessor.TabIndex = 14;
+            this.BtnLoginProfessor.Text = "Login";
+            this.BtnLoginProfessor.UseVisualStyleBackColor = true;
+            this.BtnLoginProfessor.Visible = false;
+            this.BtnLoginProfessor.Click += new System.EventHandler(this.BtnLoginProfessor_Click);
+            // 
+            // BtnStudent
+            // 
+            this.BtnStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStudent.Location = new System.Drawing.Point(317, 105);
+            this.BtnStudent.Name = "BtnStudent";
+            this.BtnStudent.Size = new System.Drawing.Size(150, 90);
+            this.BtnStudent.TabIndex = 13;
+            this.BtnStudent.Text = "Student";
+            this.BtnStudent.UseVisualStyleBackColor = true;
+            this.BtnStudent.Click += new System.EventHandler(this.BtnStudent_Click);
+            // 
+            // BtnProfessor
+            // 
+            this.BtnProfessor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnProfessor.Location = new System.Drawing.Point(67, 105);
+            this.BtnProfessor.Name = "BtnProfessor";
+            this.BtnProfessor.Size = new System.Drawing.Size(150, 89);
+            this.BtnProfessor.TabIndex = 12;
+            this.BtnProfessor.Text = "Professor";
+            this.BtnProfessor.UseVisualStyleBackColor = true;
+            this.BtnProfessor.Click += new System.EventHandler(this.BtnProfessor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(534, 311);
+            this.Controls.Add(this.BtnX2);
+            this.Controls.Add(this.BtnRegistryStudent);
+            this.Controls.Add(this.BtnLoginStudent);
+            this.Controls.Add(this.BtnX1);
+            this.Controls.Add(this.BtnRegistryProfessor);
+            this.Controls.Add(this.BtnLoginProfessor);
+            this.Controls.Add(this.BtnStudent);
+            this.Controls.Add(this.BtnProfessor);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnX2;
+        private System.Windows.Forms.Button BtnRegistryStudent;
+        private System.Windows.Forms.Button BtnLoginStudent;
+        private System.Windows.Forms.Button BtnX1;
+        private System.Windows.Forms.Button BtnRegistryProfessor;
+        private System.Windows.Forms.Button BtnLoginProfessor;
+        private System.Windows.Forms.Button BtnStudent;
+        private System.Windows.Forms.Button BtnProfessor;
     }
 }
 
--- eUcionica/Form1.cs
+++ eUcionica/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eUcionica.RegistryLoginForm;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,72 @@ namespace eUcionica
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnProfessor_Click(object sender, EventArgs e)
+        {
+            BtnProfessor.Visible = false;
+            BtnLoginProfessor.Visible = true;
+            BtnRegistryProfessor.Visible = true;
+            BtnX1.Visible = true;
+
+            BtnStudent.Visible = true;
+            BtnLoginStudent.Visible = false;
+            BtnRegistryStudent.Visible = false;
+            BtnX2.Visible = false;
+        }
+
+        private void BtnStudent_Click(object sender, EventArgs e)
+        {
+            BtnStudent.Visible = false;
+            BtnLoginStudent.Visible = true;
+            BtnRegistryStudent.Visible = true;
+            BtnX2.Visible = true;
+
+            BtnProfessor.Visible = true;
+            BtnLoginProfessor.Visible = false;
+            BtnRegistryProfessor.Visible = false;
+            BtnX1.Visible = false;
+        }
+
+        private void BtnX1_Click(object sender, EventArgs e)
+        {
+            BtnProfessor.Visible = true;
+            BtnLoginProfessor.Visible = false;
+            BtnRegistryProfessor.Visible = false;
+            BtnX1.Visible = false;
+        }
+
+        private void BtnX2_Click(object sender, EventArgs e)
+        {
+            BtnStudent.Visible = true;
+            BtnLoginStudent.Visible = false;
+            BtnRegistryStudent.Visible = false;
+            BtnX2.Visible = false;
+        }
+
+        private void BtnLoginProfessor_Click(object sender, EventArgs e)
+        {
+            ProfessorLogin form = new ProfessorLogin();
+            form.ShowDialog();
+        }
+
+        private void BtnRegistryProfessor_Click(object sender, EventArgs e)
+        {
+            ProfessorRegistry form = new ProfessorRegistry();
+            form.ShowDialog();
+        }
+
+        private void BtnLoginStudent_Click(object sender, EventArgs e)
+        {
+            StudentLogin form = new StudentLogin();
+            form.ShowDialog();
+        }
+
+        private void BtnRegistryStudent_Click(object sender, EventArgs e)
+        {
+            StudentRegistry form = new StudentRegistry();
+            form.ShowDialog();
         }
     }
 }
